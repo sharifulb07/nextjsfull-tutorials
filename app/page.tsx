aa23e25691bfbbac4151a4a7089fc47bae96da2b
sharifulb07 <sharifulb07@gmail.com>
2025-10-07 21:06:52 +0600

    client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Counter from '@/components/Counter'
 import React from 'react'
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World </h1>
+       <Counter />
     </div>
   )
 }
