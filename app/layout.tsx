bebc4f67beea02df424fea552ac06d71b41b00f9
sharifulb07 <sharifulb07@gmail.com>
2025-10-09 07:11:21 +0600

    import dynamically during runtime

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-import './globals.css';
-import {Poppins} from 'next/font/google';
-import type {Metadata} from 'next';
+import "./globals.css";
+import { Poppins } from "next/font/google";
+import type { Metadata } from "next";
 
-const poppins=Poppins({subsets:['latin'], weight:['100','200','300','400','500','600','700','800','900'],})
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
-const metadata:Metadata={
-  title:'My Nextjs  Journey',
-  description:'This is my first Nextjs project',
-}
+const metadata: Metadata = {
+  title: "My Nextjs  Journey",
+  description: "This is my first Nextjs project",
+};
 
-
-export default function Rootlayout({children}:{children:React.ReactNode}){
-
-  return(
-    <html lang='en' suppressHydrationWarning>
-      <head>
-
-      </head>
-      <body className={`${poppins}`}>
-        {children}
+export default function Rootlayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head></head>
+      <body className={"container"}>
+        <div className="wrapper">{children}</div>
       </body>
     </html>
-  )
-
+  );
 }
-
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import Counter from '@/components/Counter'
-import React from 'react'
+import dynamic from "next/dynamic";
+const LazySpike=dynamic(()=>import('@/components/Story'),{ loading:()=><h2>Sharif story is loading </h2>})
+
+// import Story from "@/components/Story";
 
 export default async function Home() {
-const res= await fetch("http://localhost:3000/api/hello")
-const data=await res.json();
 
 
   return (
     <div>
-      <h1>Hello World </h1>
-       <Counter />
-       <p>Hello Message: {data.message}</p>
+      <h3>This is shariful story now </h3>
+      <LazySpike />
+      {/* <Story /> */}
+      
     </div>
   )
 }
