95e8812f3d05ad895ad8051c5ef05c13f2c11ae3
sharifulb07 <sharifulb07@gmail.com>
2025-10-10 21:13:46 +0600

    delete method in route

--- app/comments/[id]/route.ts
+++ app/comments/[id]/route.ts
@@ -6,19 +6,33 @@ export async function GET(
 ) {
   const { id } = await params;
   const comment = Comments.find((comment) => comment.id === parseInt(id));
-  if(!comment){
-    return new Response("Comment is not Found ")
+  if (!comment) {
+    return new Response("Comment is not Found ");
   }
   return new Response(JSON.stringify(comment));
 }
 
-export async function PATCH(req:Request, {params}:{params:Promise<{id:string}>}){
+export async function PATCH(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const body = await req.json();
+  const { comment } = body;
+  const index = Comments.findIndex((comment) => comment.id === parseInt(id));
 
-    const {id}=await params;
-    const body=await req.json();
-    const {comment}=body;
-    const index=Comments.findIndex((comment)=>comment.id===parseInt(id))
+  Comments[index].comment = comment;
+  return new Response(JSON.stringify(Comments[index].comment));
+}
 
-    Comments[index].comment=comment;
-    return new Response(JSON.stringify(Comments[index].comment))
+export async function DELETE(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const body = await req.json();
+  const index = Comments.findIndex((comment) => comment.id === parseInt(id));
+  const deletedComment = Comments[index];
+  Comments.splice(index, 1);
+  return new Response(JSON.stringify(deletedComment));
 }
