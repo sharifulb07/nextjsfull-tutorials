e746a79aec31c2f90f6ebe4dff7630707b448404
sharifulb07 <sharifulb07@gmail.com>
2025-10-10 21:06:14 +0600

    partially update info of the docs now

--- app/comments/[id]/route.ts
+++ app/comments/[id]/route.ts
@@ -11,3 +11,14 @@ export async function GET(
   }
   return new Response(JSON.stringify(comment));
 }
+
+export async function PATCH(req:Request, {params}:{params:Promise<{id:string}>}){
+
+    const {id}=await params;
+    const body=await req.json();
+    const {comment}=body;
+    const index=Comments.findIndex((comment)=>comment.id===parseInt(id))
+
+    Comments[index].comment=comment;
+    return new Response(JSON.stringify(Comments[index].comment))
+}
