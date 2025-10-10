aa8dfe1870f4e8995435867acefcb01524e27916
sharifulb07 <sharifulb07@gmail.com>
2025-10-10 08:00:06 +0600

    error boundary in a certain route

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,17 @@
 import { resolve } from "path"
+import { notFound } from "next/navigation"
+
+function getRandomInt(count:number){
+  return Math.floor(Math.random()*count);
+}
 
 
 export default async function Blog() {
+
+    const realNum=getRandomInt(2);
+    if(realNum==1){
+      throw new Error("Error Loadig Review...")
+    }
 
   await new Promise((resolve)=>{
     setTimeout(()=>{
