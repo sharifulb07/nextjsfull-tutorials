f48b099264c515e83c44ec911b6163b7ba2299e0
sharifulb07 <sharifulb07@gmail.com>
2025-10-10 20:44:23 +0600

    POST METHOD

--- app/comments/route.ts
+++ app/comments/route.ts
@@ -1,6 +1,16 @@
-
 import { Comments } from "./data";
 
-export async function GET(){
-    return Response.json(Comments);
+export async function GET() {
+  return Response.json(Comments);
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const newComment = {
+    id: Comments.length + 1,
+    comment: body.comment,
+  };
+
+  Comments.push(newComment);
+  return new Response(JSON.stringify(newComment));
 }
