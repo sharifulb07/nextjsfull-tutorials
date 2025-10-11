57a5d7f9c0ef541e90a8a88affe1589554bfb108
sharifulb07 <sharifulb07@gmail.com>
2025-10-11 08:35:13 +0600

    cookie in details

--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -1,15 +1,20 @@
 import { type NextRequest } from "next/server";
 
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(req:NextRequest){
     // const customHeaders=new Headers(req.headers);
     // console.log(customHeaders.get("Authorization"))
     const headerList=await headers();
     console.log(headerList.get("Authorization"))
+
+    const cookieStore=await cookies();
+    cookieStore.set("RequestPerRate", "150")
+    console.log(cookieStore.get("RequestPerRate"))
 return new Response("<h1>Profile Api Data</h1>",{
     headers:{
-        "Content-Type":"text/html"
+        "Content-Type":"text/html",
+        "Set-Cookie":"theme=dark"
     }
 })
 }
